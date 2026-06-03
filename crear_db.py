7206500562fc8d714962cf5b54e56ce30f66bcfe
MarcosC <marcos_45cerezo@hotmail.com>
2026-06-03 09:13:41 -0300

    cargue los usuarios

--- crear_db.py
+++ crear_db.py
@@ -3,12 +3,21 @@ import sqlite3
 conn = sqlite3.connect("usuarios.db")
 
 cursor = conn.cursor()
-
-cursor.execute(
-"""
-""")
+usuarios=[
+    ("marcos","marcos"),
+    ("jere","jere"),
+    ("seba","seba"),
+    ("lucas1","lucas1"),
+    ("lucas","lucas"),
+    ("abi","abi"),
+    ("mat","mat"),
+    ("profe","profe")
+]
+cursor.executemany(
+    "INSERT INTO usuarios VALUES (?, ?)",
+    usuarios
+)
 
 conn.commit()
 conn.close()
-
-print("Base creada")
+print("usuarios cargados")
